85245a30b5a0d16b9ff92adb1c856af1ee728058
Sandro Lima <sandro.lima0016@gmail.com>
2020-07-27 18:04:37 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,14 @@ public class Program {
 		for (Seller obj : list ) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 3: Sellet findAll ====");
+		
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list ) {
+			System.out.println(obj);
+		}
 	}
 
 }
